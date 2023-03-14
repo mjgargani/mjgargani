2345f3e09ea2a77103703669550e62d5902dc2d8
Rodrigo G. Oliveira <mjgargani@gmail.com>
2023-03-14 07:55:20 -0300

    gh-pages docs deploy n03

--- src/utils/mdParser.ts
+++ src/utils/mdParser.ts
@@ -1,18 +1,21 @@
 import { marked } from 'marked'
 import htmlParser from 'html-react-parser'
-import * as DOMPurify from 'dompurify'
+import DOMPurify from 'dompurify'
 
 const mdParser = (markDown: string, exceptions?: string[]) => {
-  let result = DOMPurify.sanitize(marked.parse(markDown))
-  result = result.replaceAll(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/g, '')
-
-  if (exceptions?.length) {
-    exceptions.forEach((el) => {
-      result = result.replaceAll(el, '')
-    })
+  if(!!markDown && markDown !== "") {
+    console.log({ markDown });
+    let result = DOMPurify.sanitize(marked.parse(markDown))
+    result = result.replaceAll(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/g, '')
+  
+    if (exceptions?.length) {
+      exceptions.forEach((el) => {
+        result = result.replaceAll(el, '')
+      })
+    }
+    return htmlParser(result)
   }
-
-  return htmlParser(result)
+  return false;
 }
 
 export default mdParser
