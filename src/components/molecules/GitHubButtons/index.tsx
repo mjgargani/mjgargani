78e8d6c4d507bba9bee4ad73203d05ae8d82b54d
Rodrigo G. Oliveira <mjgargani@gmail.com>
2023-03-26 15:29:13 -0300

    remove gh-btn package

--- src/components/molecules/GitHubButtons/index.tsx
+++ src/components/molecules/GitHubButtons/index.tsx
@@ -1,34 +1,6 @@
-import GitHubButton from 'react-github-btn'
-
 import { CommonProps } from '../../../globals'
 import randomId from '../../../utils/randomId'
 import { Container } from './styles'
-
-const GitHubBtnData = {
-  common: {
-    'data-color-scheme': 'no-preference: dark; light: dark; dark: dark;',
-    'data-show-count': 'true',
-  },
-  specific: [
-    // {
-    //   href: "https://github.com/mjgargani",
-    //   "aria-label": "Follow @mjgargani on GitHub",
-    //   description: "Follow @mjgargani"
-    // },
-    // {
-    //   href: "https://github.com/mjgargani/mjgargani/subscription",
-    //   "data-icon": "octicon-eye",
-    //   "aria-label": "Watch mjgargani/mjgargani on GitHub",
-    //   description: "Watch"
-    // },
-    {
-      href: 'https://github.com/mjgargani/mjgargani',
-      'data-icon': 'octicon-star',
-      'aria-label': 'Star mjgargani/mjgargani on GitHub',
-      description: 'Star',
-    },
-  ],
-}
 
 const GitHubButtons: React.FC<CommonProps> = ({
   dataTestId = randomId('github-btns'),
@@ -51,20 +23,18 @@ const GitHubButtons: React.FC<CommonProps> = ({
             />
           </a>
         </li>
-        {GitHubBtnData.specific.map((el, i) => {
-          const props = {
-            'aria-label': el['aria-label'],
-            'data-icon': el['data-icon'],
-            href: el.href,
-          }
-          return (
-            <li key={randomId('gh-btn-item', true)}>
-              <GitHubButton {...GitHubBtnData.common} {...props}>
-                {el.description}
-              </GitHubButton>
-            </li>
-          )
-        })}
+        <li>
+          <a
+            href='https://github.com/mjgargani/mjgargani'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img
+              src='https://img.shields.io/github/stars/mjgargani/mjgargani?style=social'
+              alt='Repo stars'
+            />
+          </a>
+        </li>
       </ul>
     </Container>
   )
--- src/components/molecules/GitHubButtons/styles.ts
+++ src/components/molecules/GitHubButtons/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 import { CommonProps } from '../../../globals'
+import { device } from '../../../utils/devices'
 
 export const Container = styled.div<CommonProps>`
   position: fixed;
@@ -17,8 +18,25 @@ export const Container = styled.div<CommonProps>`
   li {
     display: inline !important;
     list-style: none !important;
-    margin-right: 0.2vw !important;
+    margin-right: 0.5vw !important;
     cursor: pointer !important;
+  }
+
+  transform: scale(.5) translate(49%, -45%);
+  @media ${device.mobileL} {
+    transform: scale(.75) translate(17%, -20%);
+  }
+  @media ${device.tablet} {
+    transform: unset;
+  }
+  @media ${device.laptopL} {
+    transform: scale(1.125) translate(-2%, 6%); 
+  }
+  @media ${device.desktop} {
+    transform: scale(1.25) translate(-5%, -10%); 
+  }
+  @media ${device.desktopL} {
+    transform: scale(1.5) translate(-10%, -15%); 
   }
 
   ${(props) => props.styledCss}
