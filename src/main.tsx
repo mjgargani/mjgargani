0ae326c2e67c943e7d3d8e9bf36610a19cf23913
Rodrigo Gargani Oliveira <mjgargani@gmail.com>
2025-03-02 14:10:40 -0300

    feat: install tailwind and postcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import { HashRouter } from 'react-router-dom';
-import FontStyles from './styles/defaults/fonts.ts';
+import FontStyles from './styles/defaults/fonts.ts'
+import './globals.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HashRouter>
-      <FontStyles />
-      <App />
-    </HashRouter>
-  </StrictMode>,
+  // <StrictMode>
+  //   <HashRouter>
+  //     <FontStyles />
+  //     <App />
+  //   </HashRouter>
+  // </StrictMode>,
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+  </h1>,
 )
